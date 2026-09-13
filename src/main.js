0e00396d527f937954697b5b8d1d5e88f404d27b
Matias Godoy <matigodoy93@hotmail.com>
2026-09-13 20:44:52 -0300

    chore: inicializar todos los módulos desde main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
 import './style.css'
+import { initRouter } from './modules/router.js'
+import { initHeroVideo } from './modules/heroVideo.js'
+import { initProjects } from './modules/projects.js'
+import { initContactForm } from './modules/contactForm.js'
+import { initSmoothScroll } from './modules/smoothScroll.js'
+import { initReveal } from './modules/reveal.js'
+
+initSmoothScroll()
+initRouter()
+initHeroVideo()
+initProjects()
+initContactForm()
+initReveal()
